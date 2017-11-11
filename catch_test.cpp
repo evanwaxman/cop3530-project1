362bf2672f6e988a6e180244f5e1da1638757322
evanwaxman <waxman.evan@gmail.com>
2017-11-11 12:02:39 -0500

    Finished adding test cases for each list function

--- catch_test.cpp
+++ catch_test.cpp
@@ -17,132 +17,110 @@
 #include "sdal.h"
 #include "cdal.h"
 
-#define TEST_SSLL 1
-#define TEST_PSSL 0
-#define TEST_SDAL 0
-#define TEST_CDAL 0
+//#define TEST_SSLL 1
+#define TEST_PSSL 1
+//#define TEST_SDAL 1
+//#define TEST_CDAL 1
 
 using namespace cop3530;
 
 /******************************************
  *   push_back
  ******************************************/
-char push_back(List<char>& list, char value) {
-    list.push_back(value);
+char push_back(List<char>& list, char element) {
+    list.push_back(element);
     return list.peek_back();
 }
 
 /******************************************
  *   push_front
  ******************************************/
-char push_front(List<char>& list, char value) {
-    list.push_front(value);
+char push_front(List<char>& list, char element) {
+    list.push_front(element);
     return list.peek_front();
 }
 
 /******************************************
- *   subtract
+ *   insert
  ******************************************/
-void subtract(List<char>& list) {
-    list.replace('e', 0);
-    //list.print();
-    
-    list.replace('z', 19);
-    //list.print();
-    
-    list.remove(0);
-    //list.print();
-    
-    list.remove(5);
-    //list.print();
-    
-    list.remove(10);
-    //list.print();
-    
-    list.remove(16);
-    //list.print();
-    
-    //std::cout << list.remove(1) << std::endl;
-    //list.print();
-    
-    list.pop_back();
-    //list.print();
-    
-    list.pop_front();
-    //list.print();
-    
-    list.push_back('b');
-    list.push_back('c');
-    list.print();
+char insert(List<char>& list, char element, size_t position) {
+    list.insert(element, position);
+    return list.item_at(position);
 }
 
 /******************************************
- *   view
+ *   replace
  ******************************************/
-void view(List<char>& list) {
-    std::cout << list.item_at(0) << std::endl;
-    std::cout << list.item_at(3) << std::endl;
-    std::cout << list.item_at(15) << std::endl;
-    //std::cout << list.item_at(16) << std::endl;
-    
-    std::cout << list.peek_back() << std::endl;
-    std::cout << list.peek_front() << std::endl;
+char replace(List<char>& list, char element, size_t position) {
+    list.replace(element, position);
+    return list.item_at(position);
 }
 
 /******************************************
- *   capacity
+ *   remove
  ******************************************/
-void capacity(List<char>& list) {
-    list.clear();
-    list.print();
-    std::cout << list.is_empty() << std::endl;
-    std::cout << list.is_full() << std::endl;
-    
-    for (size_t i=0; i<10; i++) {
-        list.push_back('a'+i);
+char remove(List<char>& list, size_t position) {
+    list.remove(position);
+    if (position == (list.length())) {
+        position--;
     }
-    list.print();
-    std::cout << list.is_empty() << std::endl;
-    std::cout << list.is_full() << std::endl;
-    
-    //list.clear();
-    list.print();
+    return list.item_at(position);
 }
 
 /******************************************
- *   contents
+ *   pop_back
  ******************************************/
-void contents(List<char>& list) {
-    list.push_back('b');
-    list.push_back('c');
-    list.push_front('a');
-    list.push_back('d');
-    
-    list.print();
-    
-    std::cout << list.contains('a') << std::endl;
-    std::cout << list.contains('d') << std::endl;
-    std::cout << list.contains('B') << std::endl;
-    std::cout << list.contains('b') << std::endl;
-    
-    char* contents = list.contents();
-    std::cout << list.contents() << std::endl;
-    delete[] contents;
+char pop_back(List<char>& list) {
+    return list.pop_back();
 }
 
 /******************************************
- *   run_tests
+ *   pop_front
  ******************************************/
-void run_tests() {
-    //add();
-    
-    //subtract(list);
-    
-    //view(list);
-    
-    //capacity(list);
-    
-    //contents(list);
+char pop_front(List<char>& list) {
+    return list.pop_front();
+}
+
+/******************************************
+ *   item_at
+ ******************************************/
+char item_at(List<char>& list, size_t position) {
+    return list.item_at(position);
+}
+
+/******************************************
+ *   peek_back
+ ******************************************/
+char peek_back(List<char>& list) {
+    return list.peek_back();
+}
+
+/******************************************
+ *   peek_front
+ ******************************************/
+char peek_front(List<char>& list) {
+    return list.peek_front();
+}
+
+/******************************************
+ *   is_empty
+ ******************************************/
+char is_empty(List<char>& list) {
+    return list.is_empty();
+}
+
+/******************************************
+ *   is_full
+ ******************************************/
+char is_full(List<char>& list) {
+    return list.is_full();
+}
+
+/******************************************
+ *   contains
+ ******************************************/
+char contains(List<char>& list, char element) {
+    return list.contains(element);
 }
 
 
@@ -161,5 +139,116 @@ TEST_CASE( "Add components to list", "[add]" ) {
     REQUIRE( push_front(list,'b') == 'b' );
     REQUIRE( push_back(list,'d') == 'd' );
     REQUIRE( push_front(list,'a') == 'a' );
+    REQUIRE( insert(list,'y',2) == 'y' );
+    REQUIRE( insert(list,'x',0) == 'x' );
+    REQUIRE( insert(list,'z',6) == 'z' );
+    
+    list.clear();   // deallocate list
 }
 
+TEST_CASE( "Subtract components from the list", "[subtract]" ) {
+    #ifdef TEST_SSLL
+        SSLL<char> list = SSLL<char>();
+    #elif TEST_PSSL
+        PSLL<char> list = PSLL<char>();
+    #elif TEST_SDAL
+        SDAL<char> list = SDAL<char>(10);
+    #elif TEST_CDAL
+        CDAL<char> list = CDAL<char>();
+    #endif
+    
+    char value = 'a';
+    for (size_t i=0; i<10; i++) {
+        push_back(list,value);
+        value++;
+    }
+    
+    REQUIRE( replace(list,'x',0) == 'x' );
+    REQUIRE( replace(list,'z',9) == 'z' );
+    REQUIRE( replace(list,'y',4) == 'y' );
+    REQUIRE( remove(list,0) == 'b' );
+    REQUIRE( remove(list,8) == 'i' );
+    REQUIRE( remove(list,3) == 'f' );
+    REQUIRE( pop_front(list) == 'b' );
+    REQUIRE( pop_back(list) == 'i' );
+    
+    list.clear();   // deallocate list
+}
+
+TEST_CASE( "View components in list", "[view]" ) {
+    #ifdef TEST_SSLL
+        SSLL<char> list = SSLL<char>();
+    #elif TEST_PSSL
+        PSLL<char> list = PSLL<char>();
+    #elif TEST_SDAL
+        SDAL<char> list = SDAL<char>(10);
+    #elif TEST_CDAL
+        CDAL<char> list = CDAL<char>();
+    #endif
+    
+    char value = 'a';
+    for (size_t i=0; i<10; i++) {
+        push_back(list,value);
+        value++;
+    }
+    
+    REQUIRE( item_at(list,0) == 'a' );
+    REQUIRE( item_at(list,9) == 'j' );
+    REQUIRE( item_at(list,4) == 'e' );
+    REQUIRE( peek_back(list) == 'j' );
+    REQUIRE( peek_front(list) == 'a' );
+    
+    list.clear();   // deallocate list
+}
+
+TEST_CASE( "Check capacity of list", "[capacity]" ) {
+    #ifdef TEST_SSLL
+        SSLL<char> list = SSLL<char>();
+    #elif TEST_PSSL
+        PSLL<char> list = PSLL<char>();
+    #elif TEST_SDAL
+        SDAL<char> list = SDAL<char>(10);
+    #elif TEST_CDAL
+        CDAL<char> list = CDAL<char>();
+    #endif
+    
+    char value = 'a';
+    for (size_t i=0; i<5; i++) {
+        push_back(list,value);
+        value++;
+    }
+    
+    REQUIRE( is_empty(list) == 0 );
+    REQUIRE( is_full(list) == 0 );
+    
+    list.clear();   // deallocate list
+    
+    REQUIRE( is_empty(list) == 1 );
+    REQUIRE( is_full(list) == 0 );
+}
+
+TEST_CASE( "Check contents of list", "[contents]" ) {
+    #ifdef TEST_SSLL
+        SSLL<char> list = SSLL<char>();
+    #elif TEST_PSSL
+        PSLL<char> list = PSLL<char>();
+    #elif TEST_SDAL
+        SDAL<char> list = SDAL<char>(10);
+    #elif TEST_CDAL
+        CDAL<char> list = CDAL<char>();
+    #endif
+    
+    char value = 'a';
+    for (size_t i=0; i<5; i++) {
+        push_back(list,value);
+        value++;
+    }
+    
+    value = 'a';
+    for (size_t i=0; i<5; i++) {
+        REQUIRE( contains(list,value) == 1 );
+        value++;
+    }
+    
+    list.clear();   // deallocate list
+}
